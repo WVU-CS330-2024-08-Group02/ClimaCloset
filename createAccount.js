0af7184f8d48399d6f3e3eece35499c6ac26212c
gma00011 <gma00011@mix.wvu.edu>
2024-10-16 11:40:00 -0400

    Update createAccount.js
    main function bare-bones outline

--- createAccount.js
+++ createAccount.js
@@ -1,4 +1,13 @@
 //Account Backend Java Script
 //main function for button click
+function userClick (){
+  if (#signupClick) {
+    //prompt to start signUp function
+  } else if (#logInClick) {
+    //prompt to start logIn function
+  } else {
+    //prompt forgot password
+  }
+};
 //login function
 //signin function
