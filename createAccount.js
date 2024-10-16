76d3fadcff044fe9d56d60426b982186bcb2e13f
krlantz <krl00018@mix.wvu.edu>
2024-10-16 11:31:21 -0400

    Update createAccount.js

--- createAccount.js
+++ createAccount.js
@@ -1,4 +1,18 @@
 //Account Backend Java Script
 //main function for button click
 //login function
+
 //signin function
+void signUp(string email){
+  
+if (matchEmail(email) == false)
+{
+  printf("Please input password:");
+  //button call
+  storeAccount(email, password);
+}
+else{
+  printf("Account already exists, please log in");
+}
+  
+}
