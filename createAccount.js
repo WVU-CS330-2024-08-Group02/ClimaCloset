0ef044d0aed018071a4c841ed2e4e0482096da96
krlantz <krl00018@mix.wvu.edu>
2024-10-20 09:41:17 -0400

    Update createAccount.js

--- createAccount.js
+++ createAccount.js
@@ -2,17 +2,23 @@
 //main function for button click
 //login function
 
-//signin function
-void signUp(string email){
-  
-if (matchEmail(email) == false)
-{
-  printf("Please input password:");
-  //button call
-  storeAccount(email, password);
+//signUp function
+void signUp(){
+  let email = prompt("What is your email?");
+
+  //checks to see if account is preexisting
+  if (matchEmail(email) == false)
+  {
+    let password = prompt("Please input password:");
+    //button call
+    storeAccount(email, password);
+  }
+  else{
+    console.log("Account already exists, please log in");
+  }
 }
-else{
-  printf("Account already exists, please log in");
-}
+
+//store account function
+void storeAccount(String emailInput, String passwordInput){
   
 }
