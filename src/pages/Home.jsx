b56e41248ea4136f40454cddee6f9d9761fbf4e1
krlantz <krl00018@mix.wvu.edu>
2024-11-02 16:07:53 -0400

    Update Home.jsx
    i added an unneccessary call to the navbar (I think)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import { useState } from "react"
-import { Navbar } from "../components/Shelf"
 import { Shelf } from "../components/Shelf"
 
 export function Home()  {
     return (
         <>
-            <Navbar />
             <h1>This is the home page</h1>
             <Shelf />
         </>
