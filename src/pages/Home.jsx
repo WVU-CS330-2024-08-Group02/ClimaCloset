6f3c95ee70ff8aa7fdc675d010be39aae7ba041b
krlantz <krl00018@mix.wvu.edu>
2024-11-02 15:58:44 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
-
+import { useState } from "react"
+import { Navbar } from "../components/Shelf"
+import { Shelf } from "../components/Shelf"
 
 export function Home()  {
     return (
         <>
+            <Navbar />
             <h1>This is the home page</h1>
+            <Shelf />
         </>
     )
 }
