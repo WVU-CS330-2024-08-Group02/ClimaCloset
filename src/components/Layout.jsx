d302c0523e70b8a3b9f0058ed99529be225814b8
mp4dev <mcp00014@mix.wvu.edu>
2024-11-02 12:44:29 -0400

    Added App icon and profile icon to navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,7 @@ export function Layout() {
     return (
         <>
             <Navbar/>
-            <main> 
+            <main style={{marginTop: "5em"}}> 
                 <Outlet/>
             </main>
         </>
