04da9e0e52147c13951f72a854dc0c04faf022bb
krlantz <krl00018@mix.wvu.edu>
2024-11-04 11:42:40 -0500

    added a weather start

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,13 @@ import { Shelf } from "../components/Shelf"
 export function Home()  {
     return (
         <>
+        <div>
             <h1>This is the home page</h1>
             <Shelf />
+        </div>
+        <div>
+            <h1>The Weather Today</h1>
+        </div>
         </>
     )
 }
