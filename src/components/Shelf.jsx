1c4f55b4446928074f74a6dc5be895187ad4cdad
krlantz <krl00018@mix.wvu.edu>
2024-11-04 11:45:56 -0500

    Added accessories to the shelf

--- src/components/Shelf.jsx
+++ src/components/Shelf.jsx
@@ -8,6 +8,7 @@ export function Shelf() {
         { id: 1, name: "Tops", description: "Displayed here will be the current tops you have in your closet." },
         { id: 2, name: "Bottoms", description: "Your current bottoms." },
         { id: 3, name: "Shoes", description: "Your current shoes." },
+        { id: 3, name: "Accessories", description: "Your current accessories." },
     ];
 
     return (
