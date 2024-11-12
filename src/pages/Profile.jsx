6d8f652e2bd82e4b1784b6a52bcfe3b30336adf7
ABC00019 <abc00019@mix.wvu.edu>
2024-11-12 14:03:54 -0500

    Start of Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,11 @@
+import React from 'react';
+import '../pages/Profile.css';
 
-
-export function Profile() {
-    return (
-        <>
-            <h1>Your profile</h1>
-        </>
-    )
+export function Profile()  {
+  return (
+      <>
+          <h1>Profile</h1>
+          <img src="/assets/CamProfile.png" alt="Profile" className='profile-image-style'/>
+      </>
+  )
 }
