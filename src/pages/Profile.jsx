b4496d1cf762ef73fc79e67f5c59b1a2ef2dd02c
ABC00019 <abc00019@mix.wvu.edu>
2024-11-13 11:51:02 -0500

    Fixes to profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,21 +4,16 @@ import '../pages/Profile.css';
 export function Profile() {
   return (
     <div className="profile-container">
-      {/* Profile Header */}
-      <div className="profile-header">
-        <img
-          src="src/assets/CamProfile.png"
-          alt="Profile"
-          className="profile-image"
-        />
-        <div className="profile-info">
-          <span className="profile-username">username</span>
-          <p className="profile-name">John Doe</p>
+      {/* Image and Username */}
+      <div className="profile-image-and-username">
+        <img src="src/assets/CamProfile.png" alt="Profile" className="profile-image"/>
+        <div className="profile-username">
+          <span>CamTheMan96</span>
         </div>
       </div>
 
       {/* Edit Button */}
-      <div className="profile-footer">
+      <div>
         <button className="edit-button">Edit Profile</button>
       </div>
     </div>
