356f2f8ede358bdc90002d474b775e439af5ba29
mp4dev <mcp00014@mix.wvu.edu>
2024-11-12 23:30:19 -0500

    Populated Profile.jsx and added css file

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,20 @@
-
+import './Login.css';
 
 export function Login()  {
     return (
         <>
-            <h1>This is the login page</h1>
+            <h1>Login to ClimaCloset</h1>
+            <div class="login-area">
+                <div>
+                    <label for="username">Enter username</label>
+                    <input type="text" id="username" placeholder="username" />
+                </div>
+                <div>
+                    <label for="password">Enter password</label>
+                    <input type="password" id="password" placeholder="password" />
+                </div>
+                <button>Submit</button>
+            </div>
         </>
     )
 }
