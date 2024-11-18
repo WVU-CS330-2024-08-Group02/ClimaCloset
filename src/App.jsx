1a66f7917417bc152f59eb98750ff0872a9549c5
mp4dev <mcp00014@mix.wvu.edu>
2024-11-18 12:32:17 -0500

    Skeleton for signup page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Weather } from './pages/Weather'
 import { Profile } from './pages/Profile'
 import { About } from './pages/About'
 import { Login } from './pages/Login'
+import { Signup } from './pages/Singup'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <Route path="/Closet" element={<Closet/>}/>
             <Route path="/Weather" element={<Weather/>}/>
             <Route path="/Login" element={<Login/>}/>
+            <Route path="/Signup" element={<Signup/>}/>
             <Route path="/Profile" element={<Profile/>}/>
             <Route path="/About" element={<About/>}/>
           </Route>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,31 @@
 import './Login.css';
+import { NavLink } from "react-router-dom"
+import { TransparentBox } from '../components/TransparentBox';
+import { CenterContainer } from '../components/CenterContainer';
 
 export function Login()  {
     return (
         <>
             <h1>Login to ClimaCloset</h1>
-            <div class="login-area">
-                <div>
-                    <label for="username">Enter username</label>
-                    <input type="text" id="username" placeholder="username" />
-                </div>
-                <div>
-                    <label for="password">Enter password</label>
-                    <input type="password" id="password" placeholder="password" />
-                </div>
-                <button>Submit</button>
-            </div>
+            <CenterContainer>
+                <TransparentBox className="login-area">
+                    <div>
+                        <label for="username">Enter username</label>
+                        <input type="text" id="username" placeholder="username" />
+                    </div>
+                    <div>
+                        <label for="password">Enter password</label>
+                        <input type="password" id="password" placeholder="password" />
+                    </div>
+                    <button>Submit</button>
+                    <p>Don't have an account? Click 
+                    <NavLink to="/Signup">
+                        <a> here </a>
+                    </NavLink>
+                    to sign up!
+                    </p>
+                </TransparentBox>
+            </CenterContainer>
         </>
     )
 }
