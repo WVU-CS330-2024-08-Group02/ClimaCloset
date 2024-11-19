9da12072540485fedb546c82ea0d52f86d640ea0
mp4dev <mcp00014@mix.wvu.edu>
2024-11-19 14:25:00 -0500

    Added use states and documentation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,20 +2,52 @@ import './Login.css';
 import { NavLink } from "react-router-dom"
 import { TransparentBox } from '../components/TransparentBox';
 import { CenterContainer } from '../components/CenterContainer';
+import { useState } from 'react';
 
+/**
+ * Login component that provides a form for users to log into ClimaCloset.
+ * 
+ * The form collects the user's username and password, with basic state management 
+ * using React's `useState` hook. Upon form submission, it displays an alert with the 
+ * entered username and password. The page also includes a link to the signup page 
+ * for users who do not have an account.
+ * 
+ * @returns {JSX.Element} A login form for the user to input credentials.
+ */
 export function Login()  {
+
+    const [username,setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    /**
+     * Handles the submission of the login form by displaying the entered
+     * username and password in an alert box.
+     * 
+     * @param {React.FormEvent} e - The form submit event.
+     */
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        alert(`Submission received.\n
+            Username: ${username}\n
+            Password: ${password}\n
+        `)
+    } 
+    
     return (
         <>
             <h1>Login to ClimaCloset</h1>
             <CenterContainer>
                 <TransparentBox>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <label for="username">Enter your username</label>
                         <input 
                             type="text" 
                             id="username"
                             required
                             placeholder="username" 
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
                         />
                         <label for="password">Enter your password</label>
                         <input 
@@ -23,6 +55,8 @@ export function Login()  {
                             id="password"
                             required
                             placeholder="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                         />
                         <button type="submit">Submit</button>
                     </form>
