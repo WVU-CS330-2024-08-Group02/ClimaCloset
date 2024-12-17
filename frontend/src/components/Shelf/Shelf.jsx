b66d3d1159bea69de0942407a231633f0d6b65e6
Cameron Hannay <114199311+cjhannay26@users.noreply.github.com>
2024-12-17 15:40:02 -0500

    Update Shelf so that it changes based on owned clothing items (successful)

--- frontend/src/components/Shelf/Shelf.jsx
+++ frontend/src/components/Shelf/Shelf.jsx
@@ -4,15 +4,25 @@ import React from 'react';
 import { useState, useEffect } from "react";
 import axios from 'axios';
 import './Shelf.css';
-import umbrella from '../../assets/clothingIcons/umbrella.png';
 import shortSleeve from '../../assets/clothingIcons/shortSleeve.png';
+import longSleeve from '../../assets/clothingIcons/Long_Sleeve.png';
+import sweater from '../../assets/clothingIcons/Sweater.png';
+import jacket from '../../assets/clothingIcons/jacket.png';
 import coat from '../../assets/clothingIcons/Coat.png';
 import jeans from '../../assets/clothingIcons/jeans.png';
+import sweatpants from '../../assets/clothingIcons/Sweatpants.png';
+import dressPants from '../../assets/clothingIcons/Dress_Pants.png';
 import shorts from '../../assets/clothingIcons/shorts.png';
 import sneakers from '../../assets/clothingIcons/sneakers.png';
 import sandals from '../../assets/clothingIcons/sandals.png';
 import boots from '../../assets/clothingIcons/boots.png';
 import flipFlops from '../../assets/clothingIcons/flipFlops.png';
+import sunglasses from '../../assets/clothingIcons/Sunglasses.png';
+import gloves from '../../assets/clothingIcons/Gloves.png';
+import scarf from '../../assets/clothingIcons/Scarf.png';
+import backpack from '../../assets/clothingIcons/Backpack.png';
+import purse from '../../assets/clothingIcons/Purse.png';
+import umbrella from '../../assets/clothingIcons/umbrella.png';
 
 /* this makes the ids for each of the thing in the closet */
 export function Shelf() {
@@ -26,8 +36,9 @@ export function Shelf() {
     useEffect(() => {
         const userId = 1;
 
-        axios.post('http://localhost:5000/api/PullCloset', { Id: userId })
+        axios.post('http://localhost:5001/closet/PullCloset', { Id: userId })
             .then(response => {
+                console.log(response.data);
                 setClothingData({ 
                     tops: response.data.tops || [],
                     bottoms: response.data.bottoms || [],
@@ -46,12 +57,12 @@ export function Shelf() {
             name: "Tops",
             items: [
                 { name: "Short_Sleeve", selected: !!clothingData.tops[0], image: shortSleeve },
-                { name: "Long_Sleeve", selected: !!clothingData.tops[1], image: shortSleeve },
+                { name: "Long_Sleeve", selected: !!clothingData.tops[1], image: longSleeve },
                 { name: "Flannel", selected: !!clothingData.tops[2], image: shortSleeve },
                 { name: "Tank_Top", selected: !!clothingData.tops[3], image: shortSleeve },
-                { name: "Sweater", selected: !!clothingData.tops[4], image: shortSleeve },
+                { name: "Sweater", selected: !!clothingData.tops[4], image: sweater },
                 { name: "Sweatshirt", selected: !!clothingData.tops[5], image: shortSleeve },
-                { name: "Jacket", selected: !!clothingData.tops[6], image: shortSleeve },
+                { name: "Jacket", selected: !!clothingData.tops[6], image: jacket },
                 { name: "Coat", selected: !!clothingData.tops[7], image: coat },
             ]
         },
@@ -60,8 +71,8 @@ export function Shelf() {
             name: "Bottoms",
             items: [
                 { name: "Jeans", selected: !!clothingData.bottoms[0], image: jeans },
-                { name: "Sweatpants", selected: !!clothingData.bottoms[1], image: shortSleeve },
-                { name: "Dress_Pants", selected: !!clothingData.bottoms[2], image: shortSleeve },
+                { name: "Sweatpants", selected: !!clothingData.bottoms[1], image: sweatpants },
+                { name: "Dress_Pants", selected: !!clothingData.bottoms[2], image: dressPants },
                 { name: "Shorts", selected: !!clothingData.bottoms[3], image: shorts },
             ]
         },
@@ -79,12 +90,12 @@ export function Shelf() {
             id: 4,
             name: "Accessories",
             items: [
-                { name: "Sunglasses", selected: !!clothingData.accessories[0], image: shortSleeve },
+                { name: "Sunglasses", selected: !!clothingData.accessories[0], image: sunglasses },
                 { name: "Hat", selected: !!clothingData.accessories[1], image: shortSleeve },
-                { name: "Gloves", selected: !!clothingData.accessories[2], image: shortSleeve },
-                { name: "Scarf", selected: !!clothingData.accessories[3], image: shortSleeve },
-                { name: "Backpack", selected: !!clothingData.accessories[4], image: shortSleeve },
-                { name: "Purse", selected: !!clothingData.accessories[5], image: shortSleeve },
+                { name: "Gloves", selected: !!clothingData.accessories[2], image: gloves },
+                { name: "Scarf", selected: !!clothingData.accessories[3], image: scarf },
+                { name: "Backpack", selected: !!clothingData.accessories[4], image: backpack },
+                { name: "Purse", selected: !!clothingData.accessories[5], image: purse },
                 { name: "Umbrella", selected: !!clothingData.accessories[6], image: umbrella },
             ]
         }
