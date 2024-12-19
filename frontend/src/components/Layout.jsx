df1829e1d5629f311ec2089b31b7a3d6e85e1f2c
gma00011 <gma00011@mix.wvu.edu>
2024-12-18 23:33:39 -0500

    Updated Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,16 +1,14 @@
+/**
+ * This file renders the navbar component and provides a consistent structure
+ * for all of the pages. This file allows for the content to be rendered dynamically 
+ * when moving between pages.
+ */
+
+// Import libraries and modules
 import { Navbar } from "./Navbar/Navbar"
 import { Outlet } from "react-router-dom"
 
-/**
- * Layout component that provides a consistent structure for the application.
- *
- * @returns {JSX.Element} A component that includes the Navbar and a main content area with dynamic rendering.
- *
- * The Layout component renders:
- * - A `Navbar` component at the top of the page.
- * - A `main` section with a top margin of 3em to prevent content overlap with the navbar.
- * - An `Outlet` component inside the main section, which acts as a placeholder for nested routes in React Router.
- */
+// Export the layout and navbar to all of the pages
 export function Layout() {
     return (
         <>
