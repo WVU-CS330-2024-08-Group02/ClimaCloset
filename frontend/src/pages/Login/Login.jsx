2b022c8b8527274eb1dd8828ce5ffc4be24aec2e
gma00011 <gma00011@mix.wvu.edu>
2024-12-19 00:01:14 -0500

    Update Login.jsx

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,3 +1,10 @@
+/**
+ * This file defines the Login page. It communicates with the backend to log-in 
+ * previous users, fetches the user's stored data, and creates the interactive elements 
+ * the user uses on the page.  
+ */
+
+// Import libraries and modules 
 import './Login.css';
 import { NavLink, useNavigate } from "react-router-dom";
 import { TransparentBox } from '../../components/TransparentBox/TransparentBox';
@@ -6,32 +13,45 @@ import { useState, useContext } from 'react';
 import { AuthContext } from "../../context/AuthContext"; // Import context
 import axios from 'axios';
 
+// Render login form and handle authentication
 export function Login() {
+    // Define variables for username & password
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const { login } = useContext(AuthContext); // Access login function from context
-    const navigate = useNavigate(); // Use navigate for redirecting
 
+    // Access login function from context
+    const { login } = useContext(AuthContext); 
+
+    // Use navigate for redirecting
+    const navigate = useNavigate(); 
+
+    // Handle form submission during login process
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         try {
+            // Send login request to server
             const response = await axios.post(`http://localhost:5001/auth/login`, 
                 { username, password }, 
                 { withCredentials: true });
             
+            // Check if login was successful
             if (response.status === 200) {
-                // Login successful
+
+                // Return successful login response and direct to homepage
                 console.log("Login successful:", response.data);
-                login(response.data.user); // Update context with user data
-                navigate('/'); // Redirect to the homepage
+                login(response.data.user); 
+                navigate('/'); 
             }
+
+        // Error handling message if login process fails
         } catch (error) {
             console.error("Error logging in:", error);
             alert("Login failed. Please check your credentials and try again.");
         }
     };
 
+    // Creation of Login page and redirects to Signup page if needed
     return (
         <>
             <h1>Login to ClimaCloset</h1>
